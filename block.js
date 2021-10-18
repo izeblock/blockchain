7743a57f3db881a23ca15217c3270dd220ac05a8
André F <andre.fischer7@gmx.de>
2021-10-18 07:29:49 +0000

    block.js ++

---
+++ block.js
@@ -0,0 +1,11 @@
+
+// Klasse
+class Block{
+    
+    constructor(timestamp,lastHash,hash,data){
+        this.timestamp = timestamp; // Zeitstempel
+        this.lastHash = lastHash; // Hash des vorhergehenden Blocks
+        this.hash = hash; // Eigener Hash
+        this.data = data; // Daten
+    }
+}
