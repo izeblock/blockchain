7c14ee27be81d65f64131729d7516dd7dbdad6f9
André F <andre.fischer7@gmx.de>
2021-10-18 08:22:12 +0000

    block.js constructor, toString

--- block.js
+++ block.js
@@ -8,4 +8,15 @@ class Block{
         this.hash = hash; // Eigener Hash
         this.data = data; // Daten
     }
+
+    toString(){ // für Debugging-Zwecke
+        return ` Block -
+            Timestamp:  ${this.timestamp}
+            Last Hash:  ${this.lastHash}
+            Hash:       ${this.hash}
+            Data:       ${this.data}
+        `
+    }
 }
+
+module.exports = Block; // Export als Modul
