dc05f300d7b89fe07036221f00436513026591f9
André F <andre.fischer7@gmx.de>
2021-10-19 08:39:55 +0000

    blockchain -> addBlock

--- blockchain.js
+++ blockchain.js
@@ -6,4 +6,11 @@ class Blockchain{
     constructor(){
         this.chain= [Block.genesis()]; // Array m. Genesis-Block
     }
+
+    addBlock(data){
+        // index = arr.length - 1
+        const lastBlock = this.chain[this.chain.length-1];
+        const block = Block.mineBlock(lastBlock,data);
+        this.chain.push(block);
+    }
 }
