9c9dc4c4e369cfb65557ff31883dd40c0faae01b
André F <andre.fischer7@gmx.de>
2021-10-19 09:09:14 +0000

    Blockchain test

--- blockchain.js
+++ blockchain.js
@@ -14,3 +14,5 @@ class Blockchain{
         this.chain.push(block);
     }
 }
+
+module.exports = Blockchain;
--- dev-test.js
+++ dev-test.js
@@ -2,12 +2,27 @@
 
 // Import
 const Block = require("./block");
+const Blockchain = require("./blockchain");
 
+/*** Test der Block-Funktionalität */
 // const block = new Block("aktuelle Zeit","hash1","hash2","meine Daten");
 // console.log(block.toString());
 
 
 // console.log(Block.genesis().toString());
 
-const testBlock = Block.mineBlock(Block.genesis(),"testDaten");
-console.log(testBlock.toString());
+// const testBlock = Block.mineBlock(Block.genesis(),"testDaten");
+// console.log(testBlock.toString());
+
+/*** Test der Blockchain-Funktionalität */
+
+const chain = new Blockchain();
+console.log(chain);
+chain.addBlock("daten von Block 1");
+console.log(chain);
+
+chain.addBlock("daten von Block 2");
+chain.addBlock("daten von Block 3");
+chain.addBlock("daten von Block 4");
+
+console.log(chain);
