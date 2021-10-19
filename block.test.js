df7fef9c65a789c43e02d3eb497c9e35778e8e6a
André F <andre.fischer7@gmx.de>
2021-10-19 10:20:09 +0000

    block.test.js structure

--- block.test.js
+++ block.test.js
@@ -1,30 +1,21 @@
 const Block = require("./block");
 
-
-
 describe("Block", () => {
-let data, lastBlock, block;
+    
+    let data, lastBlock, block;
 
+    beforeEach(() => {
+        data = "boo";
+        lastBlock = Block.genesis();
+        block = Block.mineBlock(lastBlock, data);
+    });
 
+    it("sets the data to match the input", () => {
+        expect(block.data).toEqual(data);
+    });
 
-beforeEach(() => {
-data = "boo";
-lastBlock = Block.genesis();
-block = Block.mineBlock(lastBlock, data);
-});
-
-
-
-it("sets the data to match the input", () => {
-expect(block.data).toEqual(data);
-});
-
-
-
-it("sets the lastHash to match the hash of the last block", () => {
-expect(block.lastHash).toEqual(lastBlock.hash);
-});
-
-
+    it("sets the lastHash to match the hash of the last block", () => {
+        expect(block.lastHash).toEqual(lastBlock.hash);
+    });
 
 });
